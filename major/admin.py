c138c2978c8a9ae107dfd1800c055f3db6e7ef97
zzzzty <zzzliusong@163.com>
2019-11-25 13:11:38 +0800

    解决专业信息重复的问题

--- major/admin.py
+++ major/admin.py
@@ -3,6 +3,6 @@ from .models import Major
 # Register your models here.
 @admin.register(Major)
 class MajorAdmin(admin.ModelAdmin):
-    list_display=('name','grade','create_time','last_updated_time')
+    list_display=('name','pk','grade','create_time','last_updated_time')
     search_fields = ['name']
     ordering = ['create_time',]
